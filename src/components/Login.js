470ecfe90ed53ac17659941407c71c27d06d3a30
Felipe Quintero <ffquint@gmail.com>
2021-07-17 10:35:35 -0400

    updated readme, added login form code with access to state and implemented axios post call to submit login credentials. Error message function not currently working properly. Added axios with auth helper code and private route code.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,17 +1,79 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
 
-  const error = "";
+  const initialState = {
+    credentials: {
+      username: '',
+      password: '',
+      // error: ''
+    },
+    error: ''
+  }
+
+  const [ loginForm, setLoginForm ] = useState(initialState);
+  const { push } = useHistory();
+
+  const error = loginForm.error;
+
+  // const error = e => {
+  //   this.setState({
+  //     credentials: {
+  //       ...state.credentials,
+  //       error: e.target.value
+  //     }
+  //   })
+  // };
   //replace with error state
+
+  const handleChange = e => {
+    setLoginForm({
+      credentials: {
+        ...loginForm.credentials,
+        [e.target.name]: e.target.value
+      }
+    });
+  }
+
+  const loginSubmit = e => {
+    e.preventDefault();
+    axios.post('http://localhost:5000/api/login', loginForm.credentials)
+      .then(res => {
+        localStorage.setItem('token', res.data.payload);
+        console.log("login was successful!", res);
+        // push("/protected");
+      })
+      .catch(err => console.log("Error encountered!", err))
+  };
 
   return (
     <div>
       <h1>Welcome to the Bubble App!</h1>
       <div data-testid="loginForm" className="login-form">
-        <h2>Build login form here</h2>
+        <h2>Login</h2>
+        <form onSubmit={loginSubmit}>
+          <input 
+            type="text"
+            name="username"
+            placeholder="username"
+            id="username"
+            value={error}
+            onChange={handleChange}
+          />{' '}
+          <input 
+            type="password"
+            name="password"
+            placeholder="password"
+            id="password"
+            value={error}
+            onChange={handleChange}
+          />{' '}
+          <button id="submit">Submit</button>
+        </form>
       </div>
 
       <p id="error" className="error">{error}</p>
